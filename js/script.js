815e212c558e7031f477c4edf4681282ec922379
Edyta C <edziaca@wp.pl>
2020-10-18 16:28:55 +0200

    js refactored - 4 functions defined

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
-const kwota = document.querySelector(".js-kwota");
-const kurs = document.querySelector(".js-kurs");
-const form = document.querySelector(".form");
-const wynik = document.querySelector(".js-wynik");
 
-form.addEventListener("submit", (kupa) => {
+{  
+
+const daneWynik = (a, b) => b/a ;
+
+const updateResultText = (result) => {
+    const wynik = document.querySelector(".js-wynik");
+    wynik.innerText = result;}
+
+
+const onFormSubmit = (kupa) => 
+{
     kupa.preventDefault();
+    const kwota = document.querySelector(".js-kwota");
+    const kurs = document.querySelector(".js-kurs");
+    const daneKwota = +kwota.value;
+    const daneKurs = +kurs.value;
     
-    const daneKwota = kwota.value;
-    const daneKurs = kurs.value;
-    
+    const result = daneWynik(daneKurs, daneKwota);
 
-const daneWynik = (a, b) =>  b/a;
-
-
-//const daneWynik= daneKwota/daneKurs;
-wynik.innerText = daneWynik(daneKurs, daneKwota);
-   });
+    updateResultText(result);
+   }
+const init = () =>
+{
+const form = document.querySelector(".form");
+form.addEventListener("submit", onFormSubmit);
+}
+init();
+  
+}
